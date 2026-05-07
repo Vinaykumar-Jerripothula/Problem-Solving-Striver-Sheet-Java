417525e5df5c4a7920b0fce53eee19c7f5a414d3
Vinay Kumar J <jvkumar2003@gmail.com>
2026-05-07 23:39:26 +0530

    MergeSort Added to Codebase

--- Step2/Lec2/MergeSort.java
+++ Step2/Lec2/MergeSort.java
@@ -1,5 +1,50 @@
 package Step2.Lec2;
 
+import java.util.ArrayList;
+
 public class MergeSort {
-    
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int left = low;
+        int right = mid + 1;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i=0;i<temp.size();i++) {
+            arr[low + i] = temp.get(i);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 2, 7, 1, 5, 3 ,9,3};
+
+        mergeSort(arr, 0, arr.length - 1);
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
