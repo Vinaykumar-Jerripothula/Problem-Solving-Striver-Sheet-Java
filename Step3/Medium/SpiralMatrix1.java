693d1f6187415268f807a8ad18d2c9e2d6e243c6
Vinay Kumar J <jvkumar2003@gmail.com>
2026-05-16 17:21:04 +0530

    Spiral Matrix Coded

--- Step3/Medium/SpiralMatrix1.java
+++ Step3/Medium/SpiralMatrix1.java
@@ -1,5 +1,7 @@
 
 
+import java.util.ArrayList;
+
 public class SpiralMatrix1 {
     public static void main(String[] args) {
         int[][] matrix = {
@@ -9,6 +11,37 @@ public class SpiralMatrix1 {
                 {16,17,18,19,20},
                 {21,22,23,24,25}
         };
+        ArrayList<Integer> al = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length-1;
+        int top = 0,bottom = matrix.length-1;
         
+        while(top <= bottom && left <= right) {
+            for(int i=left; i<=right;i++) {
+                al.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++) {
+                al.add(matrix[i][right]);
+            }
+            right--;
+            if(top <= bottom) {
+                for(int i=right;i >=left;i--) {
+                    al.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right) {
+                for(int i=bottom;i>=top;i--) {
+                    al.add(matrix[i][left]);
+                }
+                left++;
+            }
+        } 
+        
+        System.out.print("Spiral Order : " );
+        for(int i : al) {
+            System.out.print(i + " ");
+        }
     }    
 }
---
+++ Step3/Medium/SpiralMatrix2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class SpiralMatrix2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+
+        int cnt=1;
+        int left = 0 , right = matrix[0].length-1;
+        int top = 0 , bottom = matrix.length-1;
+
+        while(top <= bottom && left <= right) {
+
+            for(int i=left;i<=right;i++) {
+                matrix[top][i] = cnt++;
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++) {
+                matrix[i][right] = cnt++;
+            }
+            right--;
+            if(top <= bottom) {
+                for(int i=right ; i >= left ; i--) {
+                    matrix[bottom][i] = cnt++;
+                }
+                bottom--;
+            }
+            if(left <= right) {
+                for(int i=bottom; i>=top;i--) {
+                    matrix[i][left] = cnt++;
+                }
+                left++;
+            }
+
+        }
+
+        for(int[] arr : matrix) {
+            for(int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
